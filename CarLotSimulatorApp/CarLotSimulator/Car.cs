7b94818a9403cd7ad34aee7adb46dd6405e5356b
Doug Finney <dugfiny@gmail.com>
2023-01-13 10:33:31 -0600

    Added A Count To Car Lot

--- CarLotSimulatorApp/CarLotSimulator/Car.cs
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
 
 namespace CarLotSimulator
 {
-    internal class Car
+    public class Car
     {
+        //Default Constructor
+        public Car()
+        {
+            CarLot.numberofCars++;
+            Console.WriteLine($"# of Cars in Lot: {CarLot.numberofCars}");
+
+        }
+
+        //Custom Constructor
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+            CarLot.numberofCars++;
+            Console.WriteLine($"# of Cars in Lot: {CarLot.numberofCars}");
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+
+
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine($"{engineNoise}");
+        }
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine($"{honkNoise}");
+        }
+
+
     }
+
 }
--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace CarLotSimulator
 {
-    internal class CarLot
+    public class CarLot
     {
+        //Field
+        //public List<Car> carList = new List<Car>();
+
+        //Property
+
+        public List<Car> CarList { get; set; } = new List<Car>();
+
+        public static int numberofCars; 
+
+        private static void AddCar()
+        {
+            numberofCars++;
+            Console.WriteLine($"# of Cars in Lot: {numberofCars}");
+        }
     }
 }
